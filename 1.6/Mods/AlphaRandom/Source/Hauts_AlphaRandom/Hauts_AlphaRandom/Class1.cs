b9b0f90ad63b28b2807e8218a4c14d5dff3f98ec
LaserToothLiger <lasertoothliger@gmail.com>
2026-01-09 22:06:43 -0800

    v1.2.26
    The exact same mod compat for Alpha Random as existed for Alpha Genes. Gauranlen pod sprout is a good incident.

---
+++ 1.6/Mods/AlphaRandom/Source/Hauts_AlphaRandom/Hauts_AlphaRandom/Class1.cs
@@ -0,0 +1,28 @@
+﻿using AlphaRandom;
+using HautsFramework;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Hauts_AlphaRandom
+{
+    [StaticConstructorOnStartup]
+    public class Hauts_AlphaRandom
+    {
+        static Hauts_AlphaRandom()
+        {
+            BlackListedTraitsDef bltd = DefDatabase<BlackListedTraitsDef>.GetRandom();
+            foreach (TraitDef t in DefDatabase<TraitDef>.AllDefs)
+            {
+                if (t.HasModExtension<ExciseTraitExempt>() || !t.canBeSuppressed)
+                {
+                    bltd.blackListedTraits.Add(t.defName);
+                }
+            }
+        }
+    }
+}
