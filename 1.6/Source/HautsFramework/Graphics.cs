de230715a27277822bf81c78f4b9a2882aff8f0e
LaserToothLiger <lasertoothliger@gmail.com>
2026-05-14 22:38:52 -0700

    v1.4.02
    New graphical effects.

---
+++ 1.6/Source/HautsFramework/Graphics.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace HautsFramework
+{
+    public class Graphic_PawnBodySilhouette_OkForAnimalsInBed : Graphic_PawnBodySilhouette
+    {
+        public override void DrawWorker(Vector3 loc, Rot4 rot, ThingDef thingDef, Thing thing, float extraRotation)
+        {
+            if (thing is Mote mote)
+            {
+                Pawn pawn = mote.link1.Target.Thing as Pawn;
+                if (pawn == null)
+                {
+                    Corpse corpse = mote.link1.Target.Thing as Corpse;
+                    pawn = corpse.InnerPawn;
+                }
+                if (pawn.CurrentBed() != null && pawn.story == null)
+                {
+                    return;
+                }
+            }
+            base.DrawWorker(loc, rot, thingDef, thing, extraRotation);
+        }
+    }
+}
